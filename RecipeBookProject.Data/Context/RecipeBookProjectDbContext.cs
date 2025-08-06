cc0945de2896bfae217002f4b6ca63181229ac88
MuhammedAliDerindag <maliderindag@gmail.com>
2025-08-06 10:26:36 +0300

    Add project files.

---
+++ RecipeBookProject.Data/Context/RecipeBookProjectDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using RecipeBookProject.Data.Entities;
+
+namespace RecipeBookProject.Data.Context;
+
+public partial class RecipeBookProjectDbContext : DbContext
+{
+    public RecipeBookProjectDbContext(DbContextOptions<RecipeBookProjectDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ RecipeBookProject.Data/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeBookProject.Data.Entities;
+
+public partial class User
+{
+    [Key]
+    public int UserId { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string FirstName { get; set; } = null!;
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string LastName { get; set; } = null!;
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Email { get; set; } = null!;
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string PasswordHashed { get; set; } = null!;
+}
